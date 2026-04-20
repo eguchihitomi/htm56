0642311fab81c17402d5ed3e2646b38dacdd9ceb
eguchihitomi <56htm887@gmail.com>
2026-04-20 10:34:04 +0900

    Add files via upload

--- js/personal.js
+++ js/personal.js
@@ -4,11 +4,11 @@ $(function () {
     //.hide()特定のHTML要素を非表示にすること
     $(".card-list__item").hide(); // 全部非表示
     //.slice(0, 6)配列の要素を6枚ずつ取り出す .show()display: none;で非表示になっている要素を表示
-    $(".card-list__item").slice(0, 9).show();
+    $(".card-list__item").slice(0, 15).show();
     //ボタンがクリックされたとき
     $(".button").on("click", function (e) {
         e.preventDefault();
-        $(".card-list__item:hidden").slice(0, 9).slideDown();
+        $(".card-list__item:hidden").slice(0, 15).slideDown();
         //.slideDown();非表示になっている要素が、スライドしながら表示される
         //.card-list__item:hiddenがすべて表示されたら
         if ($(".card-list__item:hidden").length === 0) {
